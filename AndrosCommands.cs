c6cd3df6b57246185c2ef2027f0412f3251ed6d4
Andronomos <andronomos@proton.me>
2024-10-17 10:59:46 -0700

    Removed unused using statements

--- AndrosCommands.cs
+++ AndrosCommands.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria.ModLoader;
 
 namespace AndrosCommands
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class AndrosCommands : Mod
+    // Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
+    public class AndrosCommands : Mod
 	{
 
 	}
