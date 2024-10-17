5d3e715a23c2f8f29912f353f0c6c4a4e9eb5a90
Andronomos <andronomos@proton.me>
2024-10-17 10:59:36 -0700

    Implemented clear inventory command

---
+++ Common/Commands/ClearInventoryCommand.cs
@@ -0,0 +1,50 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace AndrosCommands.Common.Commands;
+
+public class ClearInventoryCommand : ModCommand
+{
+    public override string Command => "clearinventory";
+
+    public override string Description => "Clears the player's inventory";
+
+    public override string Usage => "/clearinventory <true|false>" +
+        "true - removes locked items" +
+        "false - skips locked items";
+
+    public override CommandType Type => CommandType.World;
+
+    public override void Action(CommandCaller caller, string input, string[] args)
+    {
+        Player player = caller.Player;
+        bool clearAll = false;
+
+        if (args.Length  > 0)
+        {
+            if (!string.IsNullOrEmpty(args[0]))
+            {
+                _ = bool.TryParse(args[0], out clearAll);
+            }
+        }
+
+        BroadcastUtils.BroadcastInfo($"{player.inventory.Length}");
+
+        for (int i = 0; i < player.inventory.Length; i++)
+        {
+            Item item = player.inventory[i];
+
+            if (item == null || item.IsAir)
+            {
+                continue;
+            }
+
+            if (!clearAll && player.inventory[i].favorited)
+            {
+                continue;
+            }
+
+            player.inventory[i] = new();
+        }
+    }
+}
