2ef66371675eb3de31428461bafdbcd56fee6e7a
Andronomos <andronomos@proton.me>
2024-10-17 08:52:35 -0700

    Initial setup for inventory purging command

---
+++ Common/Commands/PurgeInventoryCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Terraria.ModLoader;
+
+namespace AndrosCommands.Common.Commands;
+
+public class PurgeInventoryCommand : ModCommand
+{
+    public override string Command => "";
+
+    public override string Description => "";
+
+    public override string Usage => "";
+
+    public override CommandType Type => CommandType.World;
+
+    public override void Action(CommandCaller caller, string input, string[] args)
+    {
+        
+    }
+}
