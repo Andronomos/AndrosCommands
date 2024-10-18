6be3f8deddf8ecb1654e28607b942471791b504d
Andronomos <andronomos@proton.me>
2024-10-18 03:41:27 -0700

    Added method for multiple sets and refactored single set logic

--- ItemIdUtils.cs
+++ ItemIdUtils.cs
@@ -4,15 +4,33 @@ namespace AndrosCommands;
 
 public static class ItemIdUtils
 {
+    /// <summary>
+    /// Retrieves the item ids from a set
+    /// </summary>
+    /// <param name="set"></param>
+    /// <returns></returns>
     public static List<int> GetItemsInSet(bool[] set)
+    {
+        return GetItemsInSets([set]);
+    }
+
+    /// <summary>
+    /// Retrieves the item ids from multiple sets
+    /// </summary>
+    /// <param name="sets"></param>
+    /// <returns></returns>
+    public static List<int> GetItemsInSets(List<bool[]> sets)
     {
         List<int> items = [];
 
-        for (int i = 0; i < set.Length; i++)
+        foreach (bool[] set in sets)
         {
-            if (set[i])
+            for (int i = 0; i < set.Length; i++)
             {
-                items.Add(i);
+                if (set[i])
+                {
+                    items.Add(i);
+                }
             }
         }
 
