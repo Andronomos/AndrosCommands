e5a20d6923649027c0e995a493e2676fcf001827
Andronomos <andronomos@proton.me>
2024-10-27 16:06:36 -0700

    Added player class for future player commands

---
+++ Common/player/AndroPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace AndrosCommands.Common.player;
+
+public class AndroPlayer : ModPlayer
+{
+    public override void PostUpdateMiscEffects()
+    {
+        
+    }
+}
